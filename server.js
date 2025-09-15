4d807661a901ad7ae3e7b649898dc20a662e119a
Sachin Mangalick <srmangalick@gmail.com>
2025-09-15 19:03:07 -0400

    adding render support

--- server.js
+++ server.js
@@ -3,17 +3,14 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Serve static files (index.html, drawing.js)
 app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
   console.log("A user connected");
 
-  // Listen for draw events from clients
   socket.on("draw", (data) => {
-    // Broadcast to all other clients
     socket.broadcast.emit("draw", data);
   });
 
@@ -23,5 +20,5 @@ io.on("connection", (socket) => {
 });
 
 http.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
